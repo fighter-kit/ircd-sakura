8d402f341048d69161544a14ce788a6b9f28a3c2
darkwire.tl <darkwire.tl@4d035816-fc37-0410-9a16-953f3fda30e1>
2007-08-18 18:35:26 +0000

    Initial fork completed

---
+++ src/cmd_squit.cpp
@@ -0,0 +1,32 @@
+/*       +------------------------------------+
+ *       | Inspire Internet Relay Chat Daemon |
+ *       +------------------------------------+
+ *
+ *  InspIRCd: (C) 2002-2007 InspIRCd Development Team
+ * See: http://www.inspircd.org/wiki/index.php/Credits
+ *
+ * This program is free but copyrighted software; see
+ *            the file COPYING for details.
+ *
+ * ---------------------------------------------------
+ */
+
+#include "inspircd.h"
+#include "users.h"
+#include "commands/cmd_squit.h"
+
+/*
+ * This is handled by the server linking module, if necessary. Do not remove this stub.
+ */
+
+
+extern "C" DllExport command_t* init_command(InspIRCd* Instance)
+{
+	return new cmd_squit(Instance);
+}
+
+CmdResult cmd_squit::Handle (const char** parameters, int pcnt, userrec *user)
+{
+	user->WriteServ( "NOTICE %s :Look into loading a linking module (like m_spanningtree) if you want this to do anything useful.", user->nick);
+	return CMD_FAILURE;
+}

a0d4b565c3b2d614a5a76773d25bacf8a67233f2
darkwire.tl <darkwire.tl@4d035816-fc37-0410-9a16-953f3fda30e1>
2007-08-19 08:24:07 +0000

    Missed copyright

--- src/modules/m_whoismodes.cpp
+++ src/modules/m_whoismodes.cpp
@@ -2,6 +2,7 @@
 // Updated for new API - LeaChim
 // Updated for even newer API - Om
 // Updated for 1.1-beta5 - AnMaster
+// Updated for Sakura - puremind
 
 #include "users.h"
 #include "channels.h"
@@ -30,6 +31,8 @@ public:
 	virtual void OnWhois(userrec* source, userrec* dest)
 	{
 		if(*source->oper) {
+			/** changed RPL to look like in Unreal3.x, it's more pretty ;) -- puremind
+			 */
 			if (dest->modes[UM_SNOMASK] != 0) {
 				ServerInstance->SendWhoisLine(source, dest, 379, "%s %s :is using modes +%s +%s",
 				                              source->nick, dest->nick, dest->FormatModes(),
